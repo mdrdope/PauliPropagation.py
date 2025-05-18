b3128178d7bc1798f9a3f517f6336c4c74294f69
Steven Ma <ym432@cam.ac.uk>
2025-05-18 21:05:48 +0100

    Refactor: replace string-based Pauli handling with bitmask + Numba acceleration

--- pauli_pkg/tests/conftest.py
+++
@@ -1,6 +0,0 @@
-import random, numpy as np
-
-def pytest_configure():
-    # Give deterministic failures but still different random circuits each run
-    random.seed()                # system time
-    np.random.seed()
